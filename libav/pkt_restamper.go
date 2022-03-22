942d90e5ead4673460029dd9da1736eb2ec197da
Quentin Renard <contact@asticode.com>
2022-03-22 15:19:32 +0100

    Fixed pkt restamper for no pts value

--- libav/pkt_restamper.go
+++ libav/pkt_restamper.go
@@ -23,21 +23,36 @@ func newPktRestamperWithOffset() *pktRestamperWithOffset {
 	}
 }
 
-func (r *pktRestamperWithOffset) restamp(pkt *astiav.Packet, fn func(pkt *astiav.Packet) int64) {
+func (r *pktRestamperWithOffset) restamp(pkt *astiav.Packet, fn func(dts int64) int64) {
+	// Get timestamps
+	dts := r.timestamp(pkt.Dts())
+	pts := r.timestamp(pkt.Pts())
+
 	// Compute offset
 	r.m.Lock()
 	offset, ok := r.offsets[pkt.StreamIndex()]
 	if !ok {
-		offset = fn(pkt)
+		offset = fn(dts)
 		r.offsets[pkt.StreamIndex()] = offset
 	}
 	r.m.Unlock()
 
 	// Restamp
-	delta := pkt.Pts() - pkt.Dts()
-	dts := pkt.Dts() + offset
+	delta := pts - dts
+	dts += offset
 	pkt.SetDts(dts)
 	pkt.SetPts(dts + delta)
+}
+
+// We need to replace "no pts value" with a proper value otherwise it messes up the offset
+// Right now it's replaced with 0 by default since in webrtc "no pts value" is
+// equivalent to 0. But the best would be to let the caller decide which
+// should be the best default value
+func (r *pktRestamperWithOffset) timestamp(i int64) int64 {
+	if i == astiav.NoPtsValue {
+		return 0
+	}
+	return i
 }
 
 type PktRestamperStartFromZero struct {
@@ -51,7 +66,7 @@ func NewPktRestamperStartFromZero() *PktRestamperStartFromZero {
 
 // Restamp implements the Restamper interface
 func (r *PktRestamperStartFromZero) Restamp(pkt *astiav.Packet) {
-	r.restamp(pkt, func(pkt *astiav.Packet) int64 {
-		return -pkt.Dts()
+	r.restamp(pkt, func(dts int64) int64 {
+		return -dts
 	})
 }
--- libav/pkt_restamper_test.go
+++ libav/pkt_restamper_test.go
@@ -22,11 +22,11 @@ func TestPktRestamperStartFromZero(t *testing.T) {
 	defer pkt.Free()
 	r := NewPktRestamperStartFromZero()
 	for _, ft := range []pktTest{
-		{inputDts: 10, inputPts: 12, outputDts: 0, outputPts: 2, streamIdx: 1},
+		{inputDts: astiav.NoPtsValue, inputPts: astiav.NoPtsValue, outputDts: 0, outputPts: 0, streamIdx: 1},
 		{inputDts: 15, inputPts: 15, outputDts: 0, outputPts: 0, streamIdx: 2},
-		{inputDts: 20, inputPts: 23, outputDts: 10, outputPts: 13, streamIdx: 1},
+		{inputDts: 10, inputPts: 13, outputDts: 10, outputPts: 13, streamIdx: 1},
 		{inputDts: 115, inputPts: 115, outputDts: 100, outputPts: 100, streamIdx: 2},
-		{inputDts: 30, inputPts: 34, outputDts: 20, outputPts: 24, streamIdx: 1},
+		{inputDts: 20, inputPts: 24, outputDts: 20, outputPts: 24, streamIdx: 1},
 		{inputDts: 120, inputPts: 120, outputDts: 105, outputPts: 105, streamIdx: 2},
 	} {
 		pkt.SetDts(ft.inputDts)
